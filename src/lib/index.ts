2dbff7dac96c0fd750ae193143e28c8b30a9a215
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-12-10 23:38:27 +0800

    [UPDATE] / Update URL to locally hosted api

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://vcl-pass.herokuapp.com/api/'
+    // baseURL: 'https://vcl-pass.herokuapp.com/api/'
+    baseURL: 'http://localhost:5082/api/'
 });
