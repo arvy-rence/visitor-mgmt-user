50fc7bca665be92f8068203db96e437cf31af619
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-12-12 14:10:46 +0800

    [ADD] / Add User Model

---
+++ src/lib/types/User.ts
@@ -0,0 +1,16 @@
+export interface IReceivedUser {
+    barangay: string
+    birthday: Date
+    city: string
+    contact_number: string
+    created_at: Date
+    email: string
+    full_name: string
+    id: string
+    is_active: boolean
+    is_valenzuela_resident: boolean
+    province: string
+    qr_code: string
+    salt: string
+    sex: boolean
+}
