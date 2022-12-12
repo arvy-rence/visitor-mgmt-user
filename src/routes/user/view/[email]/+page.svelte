c4319df78419ce0852a216b2fdf3b3cb4b7055f5
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-12-12 14:12:27 +0800

    [ADD] / Add page for viewing the user

--- src/routes/user/view/[email]/+page.svelte
+++ src/routes/user/view/[email]/+page.svelte
@@ -3,29 +3,41 @@
 
     export let data: any;
 
+    console.log(data)
+
     const logout = async () => {
+        // logout from auth here
         await goto('/login')
+    }
+
+    const editProfile = async () => {
+        await goto(`/user/edit/${data.email}`)
     }
 </script>
 
 <div class="flex flex-col h-screen justify-center items-center">
     <h1 class="text-primary font-bold uppercase text-xl">
-        Welcome {data.email}
+        Welcome {data.full_name}
     </h1>
     <h1 class="text-primary opacity-70 text-md">
         Save your QR code to gain access to ValACE Library's facilities
     </h1>
     <div class="drop-shadow-lg">
         <div class="py-[10%]">
-            <img src="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data={data.email}&format=jpg"
+            <img src="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data={data.qr_code}&format=jpg"
                  alt="{data.email}"
                  class="px-3 py-3 border border-primary">
         </div>
     </div>
     <div class="flex flex-col justify-center items-center">
+        <button class="w-[250px] h-[2.2rem] uppercase font-bold bg-primary rounded-lg text-white my-2 inline-flex justify-center items-center"
+                on:click={editProfile}>
+            <i class="fa-solid fa-pen-to-square pr-3"></i>
+            Edit Profile
+        </button>
         <button class="w-[250px] h-[2.2rem] uppercase font-bold bg-primary rounded-lg text-white my-2">
             <a class=""
-               href="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data={data.email}&format=jpg" download="qrcode.jpg">
+               href="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data={data.qr_code}&format=jpg" download="qrcode.jpg">
                 <i class="fa-solid fa-circle-down pr-2"></i>
                 DOWNLOAD QR CODE
             </a>
